074b9d50849ae9e2d632d9ce0e2e8eb846420758
Прокопец Семен <112980469+Semen4ikk@users.noreply.github.com>
2024-02-16 11:07:35 +0300

    Add files via upload

--- Front/src/components/components.js
+++ Front/src/components/components.js
@@ -1,1 +1,93 @@
 // в данной папке создаем футер и хэдер и другие компоненты, которые используются на всех страницах
+import '../App.css'
+import {Link} from 'react-router-dom';
+import React from 'react';
+
+const Avatar = ({imageUrl}) => {
+    return (
+        <div className="avatar">
+            <img src={imageUrl} alt="Avatar"/>
+        </div>
+    );
+};
+const username = 'Лиана Алмазова';
+
+const UserName = ({ username }) => {
+    return (
+
+        <p>{username}</p>
+    );
+};
+const LoginButton = () => {
+    return (
+        <Link to="Footer" className="red_button">Войти</Link>
+    );
+};
+
+function Header() {
+    const name = String();
+
+    const AvailabilityEntrance = 0;//1 если пользователь зашел
+
+    if (AvailabilityEntrance === 1) {
+        return (
+
+            <header>
+                <h1 className="hrefki">&lt;/&gt;</h1>
+                <a className="hrefki">Все курсы</a>
+                <a className="hrefki">Поиск</a>
+                <a className="hrefki">О нас</a>
+                <Avatar imageUrl="../assets/logo.png"/>
+                <p>
+                    <UserName className="UserName" username={username}/>
+                </p>
+            </header>
+
+        )
+    } else {
+        return (
+
+            <header>
+                {/*<img src={logo}  width="100" height="auto" alt="Логотип"/>*/}
+                <h1 className="hrefki">&lt;/&gt;</h1>
+                <a className="hrefki">Все курсы</a>
+                <a className="hrefki">Поиск</a>
+                <a className="hrefki">О нас</a>
+                <LoginButton/>
+            </header>)
+    }
+}
+
+
+
+
+
+function Footer(){
+    return(
+        <footer>
+
+
+            <div>
+                <img src='../assets/images/log.jpg' alt="</>"/>
+
+            </div>
+
+
+            <ul>
+
+                <div className="link-container">
+                    <div className="link-block">
+                        <a href="#">Курсы</a>
+                        <a href="#">Напишите нам</a>
+                        <a href="#">Контакты</a>
+                    </div>
+                    <div className="link-block">
+                        <a href="#">Сотрудничество</a>
+                        <a href="#">Поиск докладчика</a>
+                        <a href="#">Присоеденяйтесь к нам</a>
+                    </div>
+                </div>
+            </ul>
+        </footer>
+    )
+}
